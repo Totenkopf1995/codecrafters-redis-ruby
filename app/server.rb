8ff58529b5b2ce3529b6716c791bc271e305dec1
totenkopf1995 <uroxacin1@hotmail.com>
2024-11-02 13:00:41 -0500

    PING PONG

--- app/server.rb
+++ app/server.rb
@@ -12,12 +12,27 @@ class YourRedisServer
     client = server.accept
 
     while (data = client.gets&.chomp) do
-      if data == 'PING'
-        client.puts("+PONG\r\n")
+      if data.start_with?('*')
+        command = parse_command(data)
+        if command == 'PING'
+          client.puts("+PONG\r\n")
+        else
+          client.puts("-ERR unknown command\r\n")
+        end
       else
-        client.puts("-ERR unknown command\r\n")
+        client.puts("-ERR invalid format\r\n")
       end
     end
+  end
+
+  private
+
+  def parse_command(data)
+    if data == "*1\r\n$4\r\nPING\r\n"
+      return 'PING'
+    end
+
+    nil
   end
 end
 
